e6f43be3daafeb54db6cf7b1e74cf8ca26ea9494
TheArcherST <mihailsapovalov05@gmail.com>
2024-06-17 02:08:50 +0300

    Make development compose default and upd readme

--- configure-prod.sh
+++ configure-prod.sh
@@ -4,5 +4,6 @@ cp ./backend/.env.sample ./backend/.env
 cp ./nginx/.env.sample ./nginx/.env
 cp ./nginx/nginx.conf.sample ./nginx/nginx.conf
 cp ./postgres/.env.sample ./postgres/.env
+cp ./frontend/.env.sample ./frontend/.env
 
 cp .env.sample .env
