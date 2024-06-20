d96f06b22502c9e591e11ff279f55be8b2f64fc7
pavel555980-debug <porsev89pi@gmail.com>
2024-06-20 11:25:00 +0300

    Added authentication to api, user_session to models

---
+++ backend/src/otvetoved_core/domain/models/user_session.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+import uuid
+
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import UUID
+
+
+from otvetoved_core.infrastructure.relational_entity import (
+    BaseRelationalEntity,
+)
+
+
+class UserSession(BaseRelationalEntity):
+    __tablename__ = "user_session"
+
+    session_token: Mapped[UUID] = mapped_column(UUID(as_uuid=True), default=uuid.uuid4, primary_key=True)
+    session_id: Mapped[int] = mapped_column(autoincrement=True)
+    user_id: Mapped[int]
