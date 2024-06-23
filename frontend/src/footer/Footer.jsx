791f566811d275751fe550c72699b82cfff68d1f
BorsalinoK1zaru <a.b.05berezovskiy@yandex.ru>
2024-06-23 22:29:03 +0300

    MainPage
    Была разработана основная страница, без реализации компонентов

---
+++ frontend/src/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Footer.css"
+ 
+
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+        <h3 className='footerText'>made by TOP academy students 2024 </h3>
+    </div>
+  )
+}
