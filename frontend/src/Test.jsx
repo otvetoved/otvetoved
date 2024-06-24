29b6aef0b002a784cf2b78011b7025ac73b2a24e
Daniil Yatsiny <maoirtx77@gmail.com>
2024-06-24 09:15:37 +0300

    cleaning for work

--- frontend/src/Test.jsx
+++ frontend/src/Test.jsx
@@ -1,25 +1,11 @@
 import React, { useState } from 'react';
-import AuthenticationModal from './components/AuthenticationModal.jsx';
-import RegistrationModal from './components/RegistrationModal.jsx';
 
-//Test.jsx используется для теста всплывающих окон, которые должны всплывать при нажатии фото профиля
 
 const Test = () => {
-  const [showLogin, setShowLogin] = useState(false);
-  const [showRegister, setShowRegister] = useState(false);
 
   return (
     <div>
 
-      <button onClick={() => setShowLogin(true)}>Фото профиля</button>
-      {showLogin && <AuthenticationModal onClose={() => setShowLogin(false)} onRegisterClick={() => {
-        setShowLogin(false);
-        setShowRegister(true);
-      }} />}
-      {showRegister && <RegistrationModal onClose={() => setShowRegister(false)} onLoginClick={() => {
-        setShowRegister(false);
-        setShowLogin(true);
-      }} />}
     </div>
   );
 };
