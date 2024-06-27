7f44bc2c22d7435006025b406f6bad8285c0bca3
Daniil Yatsiny <maoirtx77@gmail.com>
2024-06-27 14:48:41 +0300

    added static page, dynamic soon, add user.png

--- frontend/src/Test.jsx
+++ frontend/src/Test.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-
+import QuestionPage from './components/QuestionPage.jsx'
 
 const Test = () => {
 
   return (
     <div>
-
+      <QuestionPage></QuestionPage>
+      
     </div>
   );
 };
---
+++ frontend/src/components/QuestionPage.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './QuestionPage.css'
+import user from './../assets/default-user.png'
+
+const QuestionPage = () => {
+  return (
+    <div className="question-page">
+        <h2 className="h2-question">Название вопроса</h2>
+        <div className="date-question">Дата</div>
+        <div className="author-info">
+            <div className="profile">
+            <img className="user-question" src={user} alt="Аватарка" />
+            <div className="author-name">Автор темы</div>
+            </div>
+            <div className="question-info">
+            <div className="question-text">Текст вопроса</div>
+            </div>
+          <div className="answer-actions">
+            <button className="like-btn">👍 Лайк</button>
+            <button className="dislike-btn">👎 Дизлайк</button>
+          </div>
+      </div>ы
+      <div className="answer-form">
+      <div className="author-info">
+            <div className="profile">
+            <img className="user-question" src={user} alt="Аватарка" />
+            <div className="author-name">Вы</div>
+            </div>
+          <textarea placeholder="Введите ваш ответ" className="response-textarea"></textarea>
+        <button className="submit-btn">Отправить</button>
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default QuestionPage;
