633783a9ff0cdbbae56b05a008364f3b3b22742f
Mihail Shapovalov <mihailsapovalov05@gmail.com>
2024-06-29 01:07:58 +0300

    Hotfix: add missing migration file

---
+++ backend/alembic/versions/d5ec35d27ba2_add_created_at_fields.py
@@ -0,0 +1,24 @@
+"""Add created_at fields
+
+Revision ID: d5ec35d27ba2
+Revises: 645c1558197a
+Create Date: 2024-06-29 00:43:43.346738
+
+"""
+from typing import Sequence, Union
+
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'd5ec35d27ba2'
+down_revision: Union[str, None] = '645c1558197a'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
