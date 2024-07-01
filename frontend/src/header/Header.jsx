2d0d1a29bc40bc21e5b7214312f1476ad8d6a61c
Daniil Yatsiny <maoirtx77@gmail.com>
2024-07-01 10:23:42 +0300

    the connection of auth with the button on header

--- frontend/src/header/Header.jsx
+++ frontend/src/header/Header.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
-import "./Header.css"
+import React, { useState } from 'react'; 
+import './Header.css';
 import userLogo from '../assets/user.png';
- 
+import AuthenticationModal from '../components/AuthenticationModal.jsx';
+import RegistrationModal from '../components/RegistrationModal.jsx';
 
 
 export default function Header() {
+  const [showLogin, setShowLogin] = useState(false);
+  const [showRegister, setShowRegister] = useState(false);
+
   return (
     <div className='header'>
         <h1 className='siteName'>Ответовед</h1>
         <div className='user'>
             <h1 className='userText'>Account name</h1>
-            <img src={userLogo}
-                 alt="Фото пользователя"
-                 className='userLogo'
-                 />
+                <img onClick={() => setShowLogin(true)} src={userLogo} className="userLogo" alt="Profile" />
+        {showLogin && <AuthenticationModal onClose={() => setShowLogin(false)} onRegisterClick={() => {
+          setShowLogin(false);
+          setShowRegister(true);
+        }} />}
+        {showRegister && <RegistrationModal onClose={() => setShowRegister(false)} onLoginClick={() => {
+          setShowRegister(false);
+          setShowLogin(true);
+        }} />}
         </div>
     </div>
   )
