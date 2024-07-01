f5d519da53d9b4d91d859a773f65d37f0ffa275c
maoirtx77@gmail.com <maoirtx77@gmail.com>
2024-07-01 17:25:58 +0300

    create home.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,16 @@
 import React, { useState } from 'react';
+import main from "./main"
+import './styles/App.css'
 import Header from './header/Header'
 import Footer from './footer/Footer'
-import './styles/App.css'
-import QuestionCreatingPage from './components/QuestionCreatingPage'
-
-import QuestionsList from './QuestionsList'
 
 
 function App() {
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
   return (
     <>
       <Header/>
-      <div className='body'>
-      <button className='createQuestion' onClick={openModal}>Создать вопрос</button>
-      {isModalOpen && <QuestionCreatingPage onClose={closeModal} />}
-        <QuestionsList/>
-      </div>
+       <Home/>
       <Footer/>
     </>
   )
