939756bf6b8751cbb692fefa361c6b50c1effe76
Daniil Yatsiny <maoirtx77@gmail.com>
2024-07-05 15:18:29 +0300

    display users nickname

--- frontend/src/header/Header.jsx
+++ frontend/src/header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'; 
+import React, { useState, useEffect } from 'react'; 
 import './Header.css';
 import userLogo from '../assets/user.png';
 import AuthenticationModal from '../components/AuthenticationModal.jsx';
@@ -9,14 +9,37 @@ import {Link} from "react-router-dom";
 export default function Header() {
   const [showLogin, setShowLogin] = useState(false);
   const [showRegister, setShowRegister] = useState(false);
+  const [username, setUsername] = useState('');
+  const sessionToken = localStorage.getItem('sessionToken');
 
+  useEffect(() => {
+    if (sessionToken) {
+      fetch('https://otvetoved.ru/api/v1/authentication/me?session_token=' + sessionToken)
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error('Failed to fetch user information');
+          }
+        })
+        .then(data => {
+          setUsername(data.username);
+          console.log('Имя пользователя:', data.username);
+        })
+        .catch(error => {
+          console.error('Ошибка получения имени пользователя:', error);
+        });
+    }
+  }, [sessionToken]);
+
+  
   return (
     <div className='header'>
         <Link to="/" className="linkSiteName">
           <h1 className='siteName'>Ответовед</h1>
         </Link>
         <div className='user'>
-            <h1 className='userText'>Account name</h1>
+        <h1 className='userText'>{username || 'Войдитe'}</h1>
                 <img onClick={() => setShowLogin(true)} src={userLogo} className="userLogo" alt="Profile" />
         {showLogin && <AuthenticationModal onClose={() => setShowLogin(false)} onRegisterClick={() => {
           setShowLogin(false);
