0ce6220b68e0acc8fd84e1148a0be7be7741bdf9
TheArcherST <mihailsapovalov05@gmail.com>
2024-07-12 07:26:52 +0300

    Fixup

--- backend/src/otvetoved_core/presentation/admin/user_session.py
+++ backend/src/otvetoved_core/presentation/admin/user_session.py
@@ -5,7 +5,7 @@ from otvetoved_core.domain.models import UserSession
 
 class UserSessionView(ModelView, model=UserSession):
     column_list = [
-        UserSession.id,
+        UserSession.session_id,
         UserSession.user,
         UserSession.session_token,
         UserSession.created_at,
